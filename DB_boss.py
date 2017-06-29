ccdb282f8014e41fdf898b982b4aefe7f9bb6875
TVeenstra <tim.e.v@outlook.com>
2017-06-29 20:35:48 +0200

    Syntax Highlighting
    Syntax highlighting added for SQL

--- DB_boss.py
+++ DB_boss.py
@@ -8,6 +8,7 @@ import tkinter
 import sqlite3
 from tkinter.filedialog import askopenfilename
 
+
 """ ------------------------- SELECTING THE DB ---------------------------- """
 
 current_db = "fiets"
@@ -26,6 +27,52 @@ def select_db_name(current_db_label):
                                                      column=1,
                                                      columnspan=3)
     return 1
+
+""" ------------------------ SYNTAX HIGHLIGHTING ---------------------------"""
+
+def highlight_pattern(textbox, pattern, tag, start="1.0", end="end",
+                      regexp=False):
+    '''Apply the given tag to all text that matches the given pattern
+
+    If 'regexp' is set to True, pattern will be treated as a regular
+    expression.
+    
+    Original code:
+        https://stackoverflow.com/questions/34818960/how-to-highlight-a-specific-word-in-tkinter
+    '''
+
+    start = textbox.index(start)
+    end = textbox.index(end)
+    textbox.mark_set("matchStart", start)
+    textbox.mark_set("matchEnd", start)
+    textbox.mark_set("searchLimit", end)
+
+    count = tkinter.IntVar()
+    while True:
+        index = textbox.search(pattern, "matchEnd","searchLimit",
+                            count=count, regexp=regexp)
+        if index == "": break
+        textbox.mark_set("matchStart", index)
+        textbox.mark_set("matchEnd", "%s+%sc" % (index, count.get()))
+        textbox.tag_add(tag, "matchStart", "matchEnd")
+
+def highlight(textbox):
+    textbox.tag_configure("blue", foreground="blue")
+    textbox.tag_configure("orange", foreground="orange")
+    
+    highlight_pattern(textbox, "SELECT", "blue")
+    highlight_pattern(textbox, "IN", "blue")
+    highlight_pattern(textbox, "FROM", "blue")
+    highlight_pattern(textbox, "IS", "blue")
+    highlight_pattern(textbox, "WHERE", "blue")
+    highlight_pattern(textbox, "AND", "blue")
+    highlight_pattern(textbox, "OR", "blue")
+    highlight_pattern(textbox, "JOIN", "blue")
+    highlight_pattern(textbox, "UNION", "blue")
+    highlight_pattern(textbox, "CASE", "blue")
+    highlight_pattern(textbox, "WHEN", "blue")
+    highlight_pattern(textbox, "\.\w*", "orange", regexp=True)
+    
 
 """ ------------------------- HANDELING THE SQL ----------------------------"""
 
@@ -88,6 +135,7 @@ def create_start_window(window):
     """ SQL section """
     query_input= tkinter.Text(window, width=50, height=20, bg="#FFFFF0")
     query_input.grid(row=8, column=1, columnspan=3)
+    query_input.bind("<KeyRelease>", lambda event: highlight(query_input))
     seperator_label_2 = tkinter.Label(window, bg="white").grid(row=9)
     submit_query = tkinter.Button(window, text="Execute", width=57, pady=8,
                                   command=lambda: execute_query(query_input))
